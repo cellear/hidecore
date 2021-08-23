df876b7f1a9dfe13b6c5ef0800ac76fad250ee9a
Luke McCormick <lm2000@kiza.com>
2021-08-22 21:38:36 -0700

    Removing and commenting out unneeded code -- still works

--- js/hidecore.modules.js
+++ js/hidecore.modules.js
@@ -21,25 +21,7 @@
         ModuleFilter.selector = 'tbody tr';
         ModuleFilter.wrapperId = ModuleFilter.input.attr('data-table');
         ModuleFilter.wrapper = $(ModuleFilter.wrapperId);
-        var $enabled = $('.table-filter [name="checkboxes[enabled]"]', ModuleFilter.wrapper);
-        var $disabled = $('.table-filter [name="checkboxes[disabled]"]', ModuleFilter.wrapper);
-        var $unavailable = $('.table-filter [name="checkboxes[unavailable]"]', ModuleFilter.wrapper);
 
-        var showEnabled = ModuleFilter.localStorage.getBoolean('modules.enabled');
-        if (showEnabled == null) {
-          showEnabled = $enabled.is(':checked');
-        }
-        $enabled.prop('checked', showEnabled);
-        var showDisabled = ModuleFilter.localStorage.getBoolean('modules.disabled');
-        if (showDisabled == null) {
-          showDisabled = $disabled.is(':checked');
-        }
-        $disabled.prop('checked', showDisabled);
-        var showUnavailable = ModuleFilter.localStorage.getBoolean('modules.unavailable');
-        if (showUnavailable == null) {
-          showUnavailable = $unavailable.is(':checked');
-        }
-        $unavailable.prop('checked', showUnavailable);
 
         ModuleFilter.wrapper.children('details').wrapAll('<div class="modules-wrapper"></div>');
         ModuleFilter.modulesWrapper = $('.modules-wrapper', ModuleFilter.wrapper);
@@ -119,21 +101,7 @@
           },
           rules: [
             function(item) {
-              if (showEnabled) {
-                if (item.status === true && item.disabled === true) {
-                  return true;
-                }
-              }
-              if (showDisabled) {
-                if (item.status === false && item.disabled === false) {
-                  return true;
-                }
-              }
-              if (showUnavailable) {
-                if (item.status === false && item.disabled === true) {
-                  return true;
-                }
-              }
+
 
               return false;
             }
@@ -152,21 +120,7 @@
           );
         });
 
-        $enabled.change(function(e, el) {
-          showEnabled = $(this).is(':checked');
-          ModuleFilter.localStorage.setItem('modules.enabled', showEnabled);
-          ModuleFilter.winnow.filter();
-        });
-        $disabled.change(function() {
-          showDisabled = $disabled.is(':checked');
-          ModuleFilter.localStorage.setItem('modules.disabled', showDisabled);
-          ModuleFilter.winnow.filter();
-        });
-        $unavailable.change(function() {
-          showUnavailable = $unavailable.is(':checked');
-          ModuleFilter.localStorage.setItem('modules.unavailable', showUnavailable);
-          ModuleFilter.winnow.filter();
-        });
+
       }
     }
   };
